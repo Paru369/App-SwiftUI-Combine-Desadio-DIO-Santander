0194ea108a955a4a431bdb67c15625cdbc6274e5
parutech <paruvitu@gmail.com>
2023-09-04 13:08:37 -0300

    create  SwiftUI project

---
+++ iNEWs/iNEWs/iNEWsApp.swift
@@ -0,0 +1,17 @@
+//
+//  iNEWsApp.swift
+//  iNEWs
+//
+//  Created by Paulo Pinheiro on 9/4/23.
+//
+
+import SwiftUI
+
+@main
+struct iNEWsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
