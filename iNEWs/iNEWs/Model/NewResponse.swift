49e6a4975089b22182a6ea847892f7dcad9739a6
parutech <paruvitu@gmail.com>
2023-09-05 13:03:32 -0300

    create News Response Model

---
+++ iNEWs/iNEWs/Model/NewResponse.swift
@@ -0,0 +1,28 @@
+//
+//  NewResponse.swift
+//  iNEWs
+//
+//  Created by Paulo Pinheiro on 9/5/23.
+//
+
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse the JSON, add this file to your project and do:
+//
+//   let fluctuationObject = try? JSONDecoder().decode(FluctuationObject.self, from: jsonData)
+
+import Foundation
+
+// MARK: - FluctuationObject
+struct NewResponse: Codable {
+    let articles: [Article]
+}
+
+// MARK: - Article
+struct Article: Codable {
+    let author: String?
+    let url: String
+    let source, title, description: String
+    let image: String?
+    let date: Date
+}
