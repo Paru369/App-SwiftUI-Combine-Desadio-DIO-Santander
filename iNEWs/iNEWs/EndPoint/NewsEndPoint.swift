7ded8d8657c81275787dd4d1e8a2ebc644da4f53
parutech <paruvitu@gmail.com>
2023-09-05 13:29:53 -0300

    feat: create endpoint file

---
+++ iNEWs/iNEWs/EndPoint/NewsEndPoint.swift
@@ -0,0 +1,37 @@
+//
+//  NewsEndPoint.swift
+//  iNEWs
+//
+//  Created by Paulo Pinheiro on 9/5/23.
+//
+
+import Foundation
+
+protocol APIBuilder {
+    var urlRequest: URLRequest { get }
+    var baseURL: URL { get }
+    var path: String { get }
+    
+}
+
+enum NewsAPI {
+    case getNews
+}
+
+extension NewsAPI: APIBuilder {
+    
+    var baseURL: URL {
+        switch self {
+        case .getNews:
+            return URL(string: "https://api.lil.software")!
+        }
+    }
+    
+    var path: String {
+        return "/"
+    }
+    
+    var urlRequest: URLRequest {
+        return URLRequest(url: self.baseURL.appendingPathComponent(self.path))
+    }
+}
