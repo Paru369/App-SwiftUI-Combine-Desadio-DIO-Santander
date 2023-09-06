76281e72db5a54c0c2b0bc1282fe289706acc196
parutech <paruvitu@gmail.com>
2023-09-06 13:09:15 -0300

    feat: ArticleView

---
+++ iNEWs/iNEWs/Views/ArticleView.swift
@@ -0,0 +1,21 @@
+//
+//  ArticleView.swift
+//  iNEWs
+//
+//  Created by Paulo Pinheiro on 9/6/23.
+//
+
+import SwiftUI
+import URLImage
+
+struct ArticleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ArticleView_Previews: PreviewProvider {
+    static var previews: some View {
+        ArticleView()
+    }
+}
